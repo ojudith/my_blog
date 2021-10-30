e3a29ad517316cba5b63246ec4fa83c880813036
Judith Oiku <37005032+ojudith@users.noreply.github.com>
2021-10-30 20:32:20 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -53,21 +53,6 @@ return (
         ></Link> */}
         {data.site.siteMetadata.title}
       </h3>
-
-//       <Link
-//         to="/about"
-//         style={{
-//           color: `#333`,
-//           textDecoration: `none`,
-//           display: `inline`,
-//           marginRight: `5px`,
-//           border: `none !important`,
-//           marginBottom: `7px`,
-//           boxShadow: `none`,
-//         }}
-//       >
-//         <h6 className="dark-text">Portfolio</h6>
-//       </Link>
       <ThemeToggler>
         {({ theme, toggleTheme }) => (
           <label>
